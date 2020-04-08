2ef6a9bec72c68bb7377d346c17d95529c9126ea
joaopaulocampospereira <jaocampospereira@gmail.com>
2020-04-08 12:10:31 -0300

    mobile update

--- src/sevices/api.js
+++ src/sevices/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://192.168.0.111:8000'
+    baseURL: 'https://be-the-hero-semanaomnistack.herokuapp.com/'
 });
 
 export default api;
